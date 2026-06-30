1502aac53af9989fbe20eab6f09eff23faea32e9
Henry Whitaker <henrywhitaker3@outlook.com>
2026-06-30 20:31:32 +0100

    ci: better versioning and handling for new tags
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- hack/version.sh
+++ hack/version.sh
@@ -8,18 +8,19 @@ dir=$1
 sanitised=$(echo "$dir" | tr '/' '-')
 
 output=$(git cliff --include-path "$dir/*" --tag-pattern "$sanitised-v.*" --bump --unreleased --context)
+skip="false"
+changelog=$(git cliff --include-path "$dir/*" --tag-pattern "$sanitised.*" --bump)
 
 if [[ $(echo "$output" | jq -r '.[0].bump_type') == "null" ]]; then
-  echo "skip"
-  exit 0
+  skip="true"
 fi
 
-git cliff --include-path "$dir/*" --tag-pattern "$sanitised.*" --bump >"$dir/CHANGELOG.md"
-
-tag=$(echo "$output" | jq -r '.[0].version')
+tag=$(echo "$output" | jq -r '.[0].version // "bongo"')
 version=${tag#"$sanitised-"}
 
 jq -n \
   --arg tag "$tag" \
   --arg version "$version" \
-  '{"tag": $tag, "version": $version}'
+  --arg changelog "$changelog" \
+  --arg skip "$skip" \
+  '{"tag": $tag, "version": $version, "changelog": $changelog, "skip": $skip | test("true")}'
